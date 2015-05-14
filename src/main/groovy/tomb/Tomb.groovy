9d88c9cd8b6483747ef0ad36b220edfa6bdd3425
srs003es <samuel.rodriguez@diagroup.com>
2015-05-14 16:50:02 +0200

    First try to an abstrac filesystem getter

--- src/main/groovy/tomb/Tomb.groovy
+++ src/main/groovy/tomb/Tomb.groovy
@@ -13,32 +13,52 @@ import tomb.exception.FilesystemException
 
 class Tomb {
 
+    private static userValueOrDefault(def userValue, def defaultValue) {
+        userValue != null ? userValue : defaultValue
+    }
+
+    /**
+     * Returns the provider defined at config dictionary.
+     * @param config Configuration for the filesystem. It has the filesystem
+     *               type and the filesystem configuration. This configuration
+     *               depends on the underliying filesystem.
+     * @return The instance to the filesystem.
+     */
+    static FilesystemProvider getFilesystem(def config) {
+        if (config.filesystem == 'S3')
+            return getAmazonS3Filesystem(config.key, config.secret, config.bucket, Paths.get(userValueOrDefault(config.basePath, '')))
+        else if (config.filesystem == 'local')
+            return getLocalFilesystem(Paths.get(userValueOrDefault(config.basePath, '/')))
+        else
+            throw new FilesystemException("Unknown filesystem ${config.filesystem}");
+    }
+
     static FilesystemProvider getLocalFilesystem(Path basePath = Paths.get('/')) {
         File basePathFile = basePath.toFile()
-        if (!basePathFile.exists()) {
-            throw new FilesystemException("Path ${basePath} doesn't exist")
-        }
+            if (!basePathFile.exists()) {
+                throw new FilesystemException("Path ${basePath} doesn't exist")
+            }
 
         if (!basePathFile.isDirectory()) {
             throw new FilesystemException("Path ${basePath} is not a directory")
         }
 
         LocalFilesystem fs = new LocalFilesystem()
-        fs.basePath = basePath
+            fs.basePath = basePath
 
-        return fs
+            return fs
     }
 
     static FilesystemProvider getAmazonS3Filesystem(String key, String secret, String bucket, Path basePath = Paths.get('')) {
         AmazonS3Client s3Client = AmazonS3Utils.getClient(key, secret)
-        AmazonS3Utils.checkBucket(s3Client, bucket)
+            AmazonS3Utils.checkBucket(s3Client, bucket)
 
-        AmazonS3Filesystem fs = new AmazonS3Filesystem()
-        fs.basePath = basePath
-        fs.s3Client = s3Client
-        fs.bucket = bucket
+            AmazonS3Filesystem fs = new AmazonS3Filesystem()
+            fs.basePath = basePath
+            fs.s3Client = s3Client
+            fs.bucket = bucket
 
-        return fs
+            return fs
     }
 
 }
